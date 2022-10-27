637d167dd79e96401cc64e940f9ea2568ecf1102
Kantharao Juluru <94604374+kanth222@users.noreply.github.com>
2022-10-27 12:28:26 +0530

    Add files via upload

---
+++ Cy-EPV Script.ps1
@@ -0,0 +1,20 @@
+﻿[CmdletBinding()]
+param
+(
+	[Parameter(Mandatory=$true,HelpMessage="Enter the PVWA URL")]
+	[Alias("url")]
+	[String]$PVWAURL,
+
+	[Parameter(Mandatory=$false,HelpMessage="Enter the Authentication type (Default:CyberArk)")]
+	[ValidateSet("cyberark","ldap","radius")]
+	[String]$AuthType="cyberark",
+
+	# Use this switch to Disable SSL verification (NOT RECOMMENDED)
+	[Parameter(Mandatory=$false)]
+	[Switch]$DisableSSLVerify,
+
+	[Parameter(Mandatory=$false,HelpMessage="Path to a CSV file to export data to")]
+	[Alias("path")]
+	[string]$CSVPath
+)
+
